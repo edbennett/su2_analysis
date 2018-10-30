e27545f6e922fa04d0577e3e78bc55a1d49d6136
Ed Bennett <E.J.Bennett@swansea.ac.uk>
2018-10-30 10:47:37 +0000

    basic wrapper around pandas to take in files for each m and spit out a single joined table

---
+++ concatenate.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+from pandas import read_csv, concat
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('filename', nargs='+')
+    parser.add_argument('--output', required=True)
+    args = parser.parse_args()
+
+    frames = []
+
+    for filename in args.filename:
+        frames.append(read_csv(filename, delim_whitespace=True))
+
+    full_data = concat(frames)
+
+    full_data.to_csv(path_or_buf=args.output, sep='\t', na_rep='?',
+                     index=False)
+
+
+if __name__ == '__main__':
+    main()
