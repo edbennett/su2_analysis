880c7180f9a4ec6559f1501545478a273316f98d
Ed Bennett <E.J.Bennett@swansea.ac.uk>
2018-12-05 13:20:48 +0000

    more flexible correlation function fits

--- data.py
+++ data.py
@@ -3,16 +3,31 @@ from numpy import asarray
 from csv import writer, QUOTE_MINIMAL
 
 
-def write_results(filename, headers, values, channel_name=''):
+def write_results(filename, headers, values_set, channel_name='',
+                  many=False, extras_set=None):
     if channel_name:
         channel_name = f'{channel_name}_'
+    if not many:
+        values_set = (values_set,)
+    if not extras_set:
+        extras_set = [[]] * len(values_set)
+
     with open(filename, 'w', newline='') as csvfile:
-        csv_writer = writer(csvfile, delimiter='\t', quoting=QUOTE_MINIMAL)
+        csv_writer = writer(csvfile,
+                            delimiter='\t',
+                            quoting=QUOTE_MINIMAL,
+                            lineterminator='\n')
         csv_writer.writerow((f'{channel_name}{header}'
                              for header in headers))
-        csv_writer.writerow((value
-                             for value_pair in values
-                             for value in value_pair))
+#        import pdb; pdb.set_trace()
+        for values, extras in zip(values_set, extras_set):
+            csv_writer.writerow([value
+                                 for value_pair in values
+                                 for value in value_pair] + extras)
+
+
+def read_db(filename):
+    return read_csv(filename, delim_whitespace=True, na_values='?')
 
 
 def get_output_filename(basename, type, channel='', tstart='', tend='',
@@ -31,9 +46,12 @@ def get_output_filename(basename, type, channel='', tstart='', tend='',
     return f'{basename}{type}{channel}{tstart}{tend}.{filetype}'
 
 
-def get_single_raw_correlator(all_correlators, channels, NT, NS, sign=+1,
-                              ensemble_selection=0, initial_configuration=0,
-                              configuration_separation=1):
+def get_single_raw_correlator_set(all_correlators, channels, NT, NS,
+                                  valence_masses,
+                                  sign=+1,
+                                  ensemble_selection=0,
+                                  initial_configuration=0,
+                                  configuration_separation=1):
     target_correlator = concat(
         (
             all_correlators[all_correlators.channel == channel].iloc[
@@ -53,16 +71,25 @@ def get_single_raw_correlator(all_correlators, channels, NT, NS, sign=+1,
         target_correlator[column] /= 2
     target_correlator.drop(range(NT // 2 + 1, NT + 1), axis=1, inplace=True)
 
-    return NS ** 3 * target_correlator
+    correlator_set = []
+
+    for valence_mass in valence_masses:
+        correlator_set.append(
+            target_correlator[
+                target_correlator.valence_mass == valence_mass
+            ].drop(['valence_mass'], axis=1) * NS ** 3
+        )
+
+    return correlator_set
 
 
 def get_file_data(filename, NT):
     with open(filename) as f:
         number_of_columns = len(f.readline().split())
 
-    assert NT == number_of_columns - 2
+    assert NT == number_of_columns - 3
 
-    column_names = ['trajectory', 'channel'] + list(range(NT))
+    column_names = ['trajectory', 'valence_mass', 'channel'] + list(range(NT))
 
     all_correlators = read_csv(filename, names=column_names,
                                delim_whitespace=True)
@@ -76,24 +103,27 @@ def get_target_correlator(filename, channel_sets, NT, NS, signs,
     assert ensemble_selection < configuration_separation
 
     all_correlators = get_file_data(filename, NT)
+    valence_masses = sorted(set(all_correlators.valence_mass))
 
     configuration_count = (len(all_correlators) // 17 //
                            configuration_separation - 1)
 
     used_configuration_count = configuration_count - initial_configuration + 1
 
-    target_correlators = []
+    target_correlator_sets = []
 
     for channels, sign in zip(channel_sets, signs):
-        target_correlators.append(get_single_raw_correlator(
-            all_correlators, channels, NT, NS, sign,
+        target_correlator_sets.append(get_single_raw_correlator_set(
+            all_correlators, channels, NT, NS, valence_masses, sign,
             ensemble_selection, initial_configuration, configuration_separation
         ))
-        assert (len(target_correlators[-1]) - len(channels) <=
-                used_configuration_count * len(channels) <=
-                len(target_correlators[-1]))
+        for raw_correlator in target_correlator_sets[-1]:
+            assert (len(raw_correlator) - len(channels) <=
+                    used_configuration_count * len(channels) <=
+                    len(raw_correlator))
+        assert len(target_correlator_sets[-1]) == len(valence_masses)
 
-    return target_correlators
+    return map(list, zip(*target_correlator_sets)), valence_masses
 
 
 def get_flows(filename):
@@ -102,13 +132,13 @@ def get_flows(filename):
     times = asarray(sorted(set(data.t)))
     Eps = asarray(
         [
-            data[data.n == n].Ep
+            data[data.n == n].Ep.values
             for n in set(data.n)
         ]
     )
     Ecs = asarray(
         [
-            data[data.n == n].Ec
+            data[data.n == n].Ec.values
             for n in set(data.n)
         ]
     )
--- fit_correlation_function.py
+++ fit_correlation_function.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+from sys import exit
+from numpy import nan
 
 from plots import do_eff_mass_plot, do_correlator_plot, set_plot_defaults
 from data import get_target_correlator, write_results, get_output_filename
@@ -22,117 +24,126 @@ def process_correlator(
         optimizer_intensity='default', output_filename_prefix=''
 ):
     set_plot_defaults()
-    target_correlators = get_target_correlator(
+    target_correlator_sets, valence_masses = get_target_correlator(
         correlator_filename, channel_set, NT, NS, symmetries,
         ensemble_selection, initial_configuration, configuration_separation
     )
 
-    (bootstrap_mean_correlators, bootstrap_error_correlators,
-     bootstrap_correlator_samples_set) = bootstrap_correlators(
-         target_correlators, bootstrap_sample_count
-     )
+    fit_results_set = []
 
-    bootstrap_mean_eff_masses, bootstrap_error_eff_masses = (
-        bootstrap_eff_masses(bootstrap_correlator_samples_set)
-    )
+    for target_correlators, valence_mass in zip(
+            target_correlator_sets, valence_masses
+    ):
+        (bootstrap_mean_correlators, bootstrap_error_correlators,
+         bootstrap_correlator_samples_set) = bootstrap_correlators(
+             target_correlators, bootstrap_sample_count
+         )
 
-    do_eff_mass_plot(
-        bootstrap_mean_eff_masses[0],
-        bootstrap_error_eff_masses[0],
-        get_output_filename(output_filename_prefix, 'effmass', channel_name),
-        ymin=eff_mass_plot_ymin,
-        ymax=eff_mass_plot_ymax
-    )
+        bootstrap_mean_eff_masses, bootstrap_error_eff_masses = (
+            bootstrap_eff_masses(bootstrap_correlator_samples_set)
+        )
 
-    for correlator_name, channel_latex, \
-            bootstrap_mean_correlator, bootstrap_error_correlator in zip(
-            correlator_names,
-            channel_latexes,
+        do_eff_mass_plot(
+            bootstrap_mean_eff_masses[0],
+            bootstrap_error_eff_masses[0],
+            get_output_filename(output_filename_prefix, 'effmass',
+                                f'{valence_mass}_{channel_name}'),
+            ymin=eff_mass_plot_ymin,
+            ymax=eff_mass_plot_ymax
+        )
+
+        for correlator_name, channel_latex, \
+                bootstrap_mean_correlator, bootstrap_error_correlator in zip(
+                    correlator_names,
+                    channel_latexes,
+                    bootstrap_mean_correlators,
+                    bootstrap_error_correlators
+                ):
+            do_correlator_plot(
+                bootstrap_mean_correlator,
+                bootstrap_error_correlator,
+                get_output_filename(output_filename_prefix, 'correlator',
+                                    correlator_name),
+                channel_latex
+            )
+
+        if not (plateau_start and plateau_end):
+            raise Incomplete(
+                "Effective mass plot has been generated. "
+                "Now specify the start and end of the plateau to "
+                "perform the fit."
+            )
+
+        fit_results, (chisquare_value, chisquare_error) = minimize_chisquare(
+            bootstrap_correlator_samples_set,
             bootstrap_mean_correlators,
-            bootstrap_error_correlators
-            ):
-        do_correlator_plot(
-            bootstrap_mean_correlator,
-            bootstrap_error_correlator,
-            get_output_filename(output_filename_prefix, 'correlator',
-                                correlator_name),
-            channel_latex
+            fit_forms,
+            parameter_ranges,
+            plateau_start,
+            plateau_end,
+            NT,
+            fit_means=True,
+            intensity=optimizer_intensity
         )
+        fit_result_values = tuple(fit_result[0] for fit_result in fit_results)
 
-    if not (plateau_start and plateau_end):
-        raise Incomplete(
-            "Effective mass plot has been generated. "
-            "Now specify the start and end of the plateau to perform the fit."
+        for correlator_name, channel_latex, fit_form, \
+                bootstrap_mean_correlator, bootstrap_error_correlator in zip(
+                    correlator_names,
+                    channel_latexes,
+                    fit_forms,
+                    bootstrap_mean_correlators,
+                    bootstrap_error_correlators
+                ):
+            do_correlator_plot(
+                bootstrap_mean_correlator,
+                bootstrap_error_correlator,
+                get_output_filename(output_filename_prefix,
+                                    'centrally_fitted_correlator',
+                                    channel=f'{valence_mass}_{channel_name}',
+                                    tstart=plateau_start,
+                                    tend=plateau_end),
+                channel_latex,
+                fit_function=fit_form,
+                fit_params=(*fit_result_values, NT),
+                fit_legend='Fit of central values',
+                t_lowerbound=plateau_start - 3.5,
+                t_upperbound=plateau_end - 0.5,
+                corr_upperbound=correlator_upperbound,
+                corr_lowerbound=correlator_lowerbound
+            )
+
+        fit_results, (chisquare_value, chisquare_error) = minimize_chisquare(
+            bootstrap_correlator_samples_set,
+            bootstrap_mean_correlators,
+            fit_forms,
+            parameter_ranges,
+            plateau_start,
+            plateau_end,
+            NT,
+            fit_means=False
         )
+        (mass, mass_error), *_ = fit_results
+        fit_results_set.append((fit_results,
+                                (chisquare_value, chisquare_error)))
 
-    fit_results, (chisquare_value, chisquare_error) = minimize_chisquare(
-        bootstrap_correlator_samples_set,
-        bootstrap_mean_correlators,
-        fit_forms,
-        parameter_ranges,
-        plateau_start,
-        plateau_end,
-        NT,
-        fit_means=True,
-        intensity=optimizer_intensity
-    )
-    fit_result_values = tuple(fit_result[0] for fit_result in fit_results)
-
-    for correlator_name, channel_latex, fit_form, \
-            bootstrap_mean_correlator, bootstrap_error_correlator in zip(
-                correlator_names,
-                channel_latexes,
-                fit_forms,
-                bootstrap_mean_correlators,
-                bootstrap_error_correlators
-            ):
-        do_correlator_plot(
-            bootstrap_mean_correlator,
-            bootstrap_error_correlator,
+        do_eff_mass_plot(
+            bootstrap_mean_eff_masses[0],
+            bootstrap_error_eff_masses[0],
             get_output_filename(output_filename_prefix,
-                                'centrally_fitted_correlator',
-                                channel=channel_name,
+                                'effmass_withfit',
+                                channel=f'{valence_mass}_{channel_name}',
                                 tstart=plateau_start,
                                 tend=plateau_end),
-            channel_latex,
-            fit_function=fit_form,
-            fit_params=(*fit_result_values, NT),
-            fit_legend='Fit of central values',
-            t_lowerbound=plateau_start - 3.5,
-            t_upperbound=plateau_end - 0.5,
-            corr_upperbound=correlator_upperbound,
-            corr_lowerbound=correlator_lowerbound
+            ymin=eff_mass_plot_ymin,
+            ymax=eff_mass_plot_ymax,
+            m=mass,
+            m_error=mass_error,
+            tmin=plateau_start - 0.5,
+            tmax=plateau_end - 0.5
         )
 
-    fit_results, (chisquare_value, chisquare_error) = minimize_chisquare(
-        bootstrap_correlator_samples_set,
-        bootstrap_mean_correlators,
-        fit_forms,
-        parameter_ranges,
-        plateau_start,
-        plateau_end,
-        NT,
-        fit_means=False
-    )
-    (mass, mass_error), *_ = fit_results
-
-    do_eff_mass_plot(
-        bootstrap_mean_eff_masses[0],
-        bootstrap_error_eff_masses[0],
-        get_output_filename(output_filename_prefix,
-                            'effmass_withfit',
-                            channel=channel_name,
-                            tstart=plateau_start,
-                            tend=plateau_end),
-        ymin=eff_mass_plot_ymin,
-        ymax=eff_mass_plot_ymax,
-        m=mass,
-        m_error=mass_error,
-        tmin=plateau_start - 0.5,
-        tmax=plateau_end - 0.5
-    )
-
-    return fit_results, (chisquare_value, chisquare_error)
+    return fit_results_set, valence_masses
 
 
 def main():
@@ -158,6 +169,8 @@ def main():
     parser.add_argument('--optimizer_intensity', default='default',
                         choices=('default', 'intense'))
     parser.add_argument('--output_filename_prefix', default=None)
+    parser.add_argument('--ignore', action='store_true')
+    parser.add_argument('--no_decay_const', action='store_true')
     args = parser.parse_args()
 
     if not args.output_filename_prefix:
@@ -203,6 +216,14 @@ def main():
         'g5gk': ('mass', 'mass_error', 'decay_const', 'decay_const_error',
                  'chisquare', 'chisquare_error'),
     }
+    if args.no_decay_const and channel_name == 'g5':
+        channel_set_options['g5'] = (('g5',),)
+        correlator_names_options['g5']  = ('g5',)
+        channel_latexes_options['g5'] = (r'\gamma_5,\gamma_5',)
+        fit_forms_options['g5'] = (v_fit_form,)
+        output_file_header_options['g5'] = output_file_header_options['gk']
+        symmetries_options['g5'] = symmetries_options['gk']
+        parameter_range_options['g5'] = parameter_range_options['gk']
 
     channel_set = channel_set_options[channel_name]
     correlator_names = correlator_names_options[channel_name]
@@ -211,8 +232,21 @@ def main():
     symmetries = symmetries_options[channel_name]
     parameter_ranges = parameter_range_options[channel_name]
 
+    if args.ignore:
+        print("Ignored as requested")
+        write_results(
+            filename=get_output_filename(
+                args.output_filename_prefix, 'mass', channel_name,
+                filetype='dat'
+            ),
+            channel_name=channel_name,
+            headers=output_file_header_options[channel_name],
+            values_set=[(nan, nan) for _ in parameter_ranges] + [(nan, nan)]
+        )
+        exit()
+
     try:
-        fit_results = process_correlator(
+        fit_results_set, valence_masses = process_correlator(
             args.correlator_filename,
             channel_name, channel_set, channel_latexes, symmetries,
             correlator_names, fit_forms, args.NT, args.NS,
@@ -233,32 +267,46 @@ def main():
 
     except Incomplete as ex:
         print("ANALYSIS NOT YET COMPLETE")
-        print(ex.desc)
+        print(ex)
     else:
-        mass, mass_error = fit_results[0][0]
-        decay_const, decay_const_error = fit_results[0][1]
-        if len(fit_results[0]) > 2:
-            amplitude, amplitude_error = fit_results[0][2]
-        chisquare_value, chisquare_error = fit_results[1]
+
+        if len(valence_masses) > 1:
+            filetype = 'long_dat'
+            extras_set = (valence_masses,)
+        else:
+            filetype = 'dat'
+            extras_set = ()
 
         write_results(
             filename=get_output_filename(
                 args.output_filename_prefix, 'mass', channel_name,
-                filetype='dat'
+                filetype=filetype
             ),
             channel_name=channel_name,
             headers=output_file_header_options[channel_name],
-            values=(*fit_results[0], fit_results[1])
+            values_set=[(*fit_results[0], fit_results[1])
+                        for fit_results in fit_results_set],
+            many=True,
+            extras_set=extras_set
         )
         if not args.silent:
-            print(f'{channel_name} mass: {mass} ± {mass_error}')
-            print(f'{channel_name} decay constant: '
-                  f'{decay_const} ± {decay_const_error}')
-            if len(fit_results[0]) > 2:
-                print(f'{channel_name} amplitude: '
-                      f'{amplitude} ± {amplitude_error}')
-            print(f'{channel_name} chi-square: '
-                  f'{chisquare_value} ± {chisquare_error}')
+            for valence_mass, fit_results in zip(
+                    valence_masses, fit_results_set
+            ):
+                mass, mass_error = fit_results[0][0]
+                decay_const, decay_const_error = fit_results[0][1]
+                if len(fit_results[0]) > 2:
+                    amplitude, amplitude_error = fit_results[0][2]
+                chisquare_value, chisquare_error = fit_results[1]
+
+                print(f'{channel_name} mass: {mass} ± {mass_error}')
+                print(f'{channel_name} decay constant: '
+                      f'{decay_const} ± {decay_const_error}')
+                if len(fit_results[0]) > 2:
+                    print(f'{channel_name} amplitude: '
+                          f'{amplitude} ± {amplitude_error}')
+                print(f'{channel_name} chi-square: '
+                      f'{chisquare_value} ± {chisquare_error}')
 
 
 if __name__ == '__main__':
