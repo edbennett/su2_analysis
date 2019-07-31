2c9092706707d824517c29dfc4792f7472e3b308
Ed Bennett <e.j.bennett@swansea.ac.uk>
2019-07-31 22:13:31 +0100

    add table 6 to table5.py and rename

--- autospn/table_specs/table5_6.py
+++ autospn/table_specs/table5_6.py
@@ -15,7 +15,7 @@ ERROR_DIGITS = 2
 EXPONENTIAL = False
 
 
-def generate(data):
+def table5(data):
     columns = ['', None, r'$am_{\mathrm{PS}}$', r'$af_{\mathrm{PS}}$',
                r'$am_{\mathrm{S}}$', None,
                r'$m_{\mathrm{PS}}L$', r'$f_{\mathrm{PS}}L$']
@@ -41,3 +41,27 @@ def generate(data):
         error_digits=ERROR_DIGITS,
         exponential=EXPONENTIAL
     )
+
+
+def table6(data):
+    columns = ['', None, r'$am_{\mathrm{V}}$', r'$af_{\mathrm{V}}$', None,
+               r'$am_{\mathrm{AV}}$', r'$af_{\mathrm{AV}}$', None,
+               r'$am_{\mathrm{T}}$', r'$am_{\mathrm{AT}}$']
+    observables = ('gk_mass', 'gk_decay_const', 'g5gk_mass',
+                   'g5gk_decay_const', 'g0gk_mass', 'g0g5gk_mass')
+    filename = 'table6.tex'
+
+    generate_table_from_db(
+        data=data,
+        ensembles=ENSEMBLES,
+        observables=observables,
+        filename=filename,
+        columns=columns,
+        error_digits=ERROR_DIGITS,
+        exponential=EXPONENTIAL
+    )
+
+
+def generate(data):
+    table5(data)
+    table6(data)
