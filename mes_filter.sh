c55331147ff9440f01dfdf8223d89339c2932040
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-07-29 21:31:51 +0100

    remove leftover scripts

--- mes_filter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep TRIPLET | grep -v _im= | grep -v _disc= | perl -n -e's/\[MAIN\]\[0\]conf #\d+ mass=(-*[\.\d]+)( DEFAULT_SEMWALL)? TRIPLET/$1/g; chomp; s/=//g; s/[A-Z_]* TRIPLET//g; print "$_\n" unless ($_=~/^\s?$/);' | perl -n -e'$a=int(($.-1)/17); print "$a $_";'
