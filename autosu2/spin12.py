49cbdb09d67b13258d245835c550ab5715fae54d
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-08-14 15:30:59 +0100

    correctly cope with new structure of having multiple spin12 files for different streams when making data package

--- autosu2/spin12.py
+++ autosu2/spin12.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import builtins
 from collections import defaultdict
+from glob import glob
 
-from numpy import asarray
+import numpy as np
 
 
 def tuple_to_complex(tuple_string):
@@ -75,7 +77,7 @@ def arrayify_corr(result):
     )
     new_result = {"metadata": {}}
     for attr in metadata:
-        new_result["metadata"][attr] = asarray(
+        new_result["metadata"][attr] = np.asarray(
             [cfg_result["metadata"][attr] for _, cfg_result in sorted(result.items())]
         )
 
@@ -83,60 +85,106 @@ def arrayify_corr(result):
         "metadata"
     }
     for channel in channels:
-        new_result[channel] = asarray(
+        new_result[channel] = np.asarray(
             [cfg_result[channel] for _, cfg_result in sorted(result.items())]
         )
     return new_result
 
 
-def arrayify(results):
-    new_results = {}
-    for observable, observable_results in results.items():
-        if observable.endswith("plaquette") or observable.startswith("polyakov_line"):
-            new_results[observable] = asarray(
-                [value for _, value in sorted(observable_results.items())]
-            )
-        elif observable in ("Meson_corr", "gluinoglue"):
-            new_results[observable] = arrayify_corr(observable_results)
-        else:
-            new_results[observable] = observable_results
-    return new_results
+def recursive_concatenate_dicts(to_concatenate):
+    result = {}
+    keys = set([key for single_dict in to_concatenate for key in single_dict])
+    for key in keys:
+        valid_subset = [
+            single_dict[key] for single_dict in to_concatenate if key in single_dict
+        ]
+        types = set(map(type, valid_subset))
+        if len(types) > 1:
+            raise ValueError(f"Concatenation type mismatch: {types}")
+
+        match types.pop():
+            case np.ndarray:
+                result[key] = np.concatenate(valid_subset)
+            case builtins.dict:
+                result[key] = recursive_concatenate_dicts(valid_subset)
+            case invalid_type:
+                raise ValueError(f"Don't know how to concatenate {invalid_type}")
+
+    return result
 
 
-def get_correlators_spin12format(filename):
-    results = defaultdict(lambda: defaultdict(dict))
+def arrayify(all_results):
+    all_new_results = []
+    for results_idx, results in enumerate(all_results):
+        new_results = {}
+        for observable, observable_results in results.items():
+            if observable.endswith("plaquette") or observable.startswith(
+                "polyakov_line"
+            ):
+                new_results[observable] = np.asarray(
+                    [value for _, value in sorted(observable_results.items())]
+                )
+            elif observable in ("Meson_corr", "gluinoglue"):
+                new_results[observable] = arrayify_corr(observable_results)
+            elif observable == "trajectories":
+                new_results[observable] = np.vstack(
+                    [
+                        [results_idx] * len(observable_results),
+                        observable_results,
+                    ]
+                ).T
+            else:
+                new_results[observable].append(observable_results)
 
-    cfg_indices = set()
+        all_new_results.append(new_results)
 
-    for i, line in enumerate(open(filename, "r")):
-        split_line = line.split(":")
-        if (not split_line) or split_line[0] != "(MM)":
-            continue
-        cfg_index = int(split_line[1])
-        cfg_indices.add(cfg_index)
+    return recursive_concatenate_dicts(all_new_results)
 
-        observable = split_line[2]
-        if observable.endswith("plaquette"):
-            results[observable][cfg_index] = float(split_line[3])
-        elif observable.startswith("polyakov_line"):
-            results[observable][cfg_index] = tuple_to_complex(split_line[3])
-        elif observable == "Meson_corr":
-            process_meson_corr_line(split_line, results[observable][cfg_index])
-        elif observable == "gluinoglue":
-            process_gluinoglue_line(split_line, results[observable][cfg_index])
 
-    # Ensure that everything has a result
-    keys_to_remove = []
-    for observable_key, observable_results in results.items():
-        # Some datasets are missing a large chunk of mesonic correlators
-        # In order to maintain the data structure we have created elsewhere
-        # ignore the mesonic correlators for these data
-        if len(observable_results) < 0.8 * len(cfg_indices):
-            keys_to_remove.append(observable_key)
-        else:
-            assert set(observable_results.keys()) == cfg_indices
+def get_correlators_spin12format(filename_base):
+    all_results = []
 
-    for key in keys_to_remove:
-        del results[key]
-    results["trajectories"] = asarray(sorted(cfg_indices))
-    return arrayify(results)
+    for filename in glob(f"{filename_base}*"):
+        cfg_indices = []
+        results = defaultdict(lambda: defaultdict(dict))
+        for i, line in enumerate(open(filename, "r")):
+            split_line = line.split(":")
+            if (not split_line) or (split_line[0] != "(MM)") or (len(split_line) < 3):
+                continue
+            cfg_index = int(split_line[1])
+            cfg_indices.append(cfg_index)
+
+            observable = split_line[2]
+            if observable.endswith("plaquette"):
+                results[observable][cfg_index] = float(split_line[3])
+            elif observable.startswith("polyakov_line"):
+                results[observable][cfg_index] = tuple_to_complex(split_line[3])
+            elif observable == "Meson_corr":
+                process_meson_corr_line(split_line, results[observable][cfg_index])
+            elif observable == "gluinoglue":
+                process_gluinoglue_line(split_line, results[observable][cfg_index])
+
+        # Ensure that everything has a result
+        keys_to_remove = []
+        for observable_key, observable_results in results.items():
+            # Some datasets are missing a large chunk of mesonic correlators
+            # In order to maintain the data structure we have created elsewhere
+            # ignore the mesonic correlators for these data
+            if len(set(observable_results)) < 0.8 * len(set(cfg_indices)):
+                keys_to_remove.append(observable_key)
+            # Sometimes the last trajectory starts but does not complete
+            elif set(cfg_indices) - set(observable_results.keys()) == {cfg_indices[-1]}:
+                last_cfg_idx = cfg_indices.pop()
+                for _, other_observable_results in results.items():
+                    if last_cfg_idx in other_observable_results:
+                        del other_observable_results[last_cfg_idx]
+            else:
+                assert set(observable_results.keys()) == set(cfg_indices)
+
+        for key in keys_to_remove:
+            del results[key]
+        results["trajectories"] = np.asarray(sorted(set(cfg_indices)))
+
+        all_results.append(results)
+
+    return arrayify(all_results)
