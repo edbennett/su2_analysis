697ace7393722e1324c5f4b1c062539d0ce3816e
Ed Bennett <e.j.bennett@swansea.ac.uk>
2025-06-11 12:22:52 +0100

    get aic plots for Nf=2

--- autosu2/plot_specs/collate_aic.py
+++ autosu2/plot_specs/collate_aic.py
@@ -12,3 +12,11 @@ def generate(data, ensembles):
         "processed_data/nf1_ADJ/b2.2/m-1.378/48x24/modenumber_slice_plot.pdf",
         "assets/plots/DB4M11_modenumber_slice_plot.pdf",
     )
+    shutil.copyfile(
+        "processed_data/nf2_ADJ/b2.3/m-1.14/96x48/modenumber_slice_plot.pdf",
+        "assets/plots/Nf2DB1M13_modenumber_slice_plot.pdf",
+    )
+    shutil.copyfile(
+        "processed_data/nf2_ADJ/b2.3/m-1.14/96x48/modenumber_grad_plot.pdf",
+        "assets/plots/Nf2DB1M13_modenumber_grad_plot.pdf",
+    )
